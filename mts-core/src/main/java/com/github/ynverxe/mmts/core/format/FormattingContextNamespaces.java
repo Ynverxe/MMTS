ad244b713ce3e6b082c8a388bcc19b967824a92a
Nahuel <68520721+ImTheNico@users.noreply.github.com>
2022-11-03 16:11:48 -0300

    refactor: add new namespace for replacement values

--- mts-core/src/main/java/com/github/ynverxe/mmts/core/format/FormattingContextNamespaces.java
+++ mts-core/src/main/java/com/github/ynverxe/mmts/core/format/FormattingContextNamespaces.java
@@ -2,8 +2,9 @@ package com.github.ynverxe.mmts.core.format;
 
 public final class FormattingContextNamespaces {
 
+    public static final String REPLACEMENT_VALUES = "replacements_values";
     public static final String REMITTENT_NAMESPACE = "message_remittent";
-    public static final String PLACEHOLDER_DELIMITER_NAMESPACE = "placeholder_delimiter_pack";
+    public static final String PLACEHOLDER_DELIMITER = "placeholder_delimiter_pack";
     public static final String SKIP_PLACEHOLDER_APPLICATION = "skip_placeholder_formatting";
 
 }
